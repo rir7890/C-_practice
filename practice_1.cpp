b84d3dfe29c2c4291b9785919d0894de067b1895
rir7890 <Rahul Kumar>
2023-11-28 08:31:18 +0530

    count the number of subarray with given xor K

--- practice_1.cpp
+++ practice_1.cpp
@@ -1,6 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// int subarraysWithXorK(vector<int> &a, int k)
+// {
+//     int n = a.size();
+//     int xr = 0;
+//     map<int, int> mpp;
+//     mpp[xr]++;
+//     int cnt = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         xr = xr ^ a[i];
+//         int r = xr ^ k;
+//         cnt += mpp[r];
+//         mpp[xr]++;
+//     }
+//     return cnt;
+// }
+// int main()
+// {
+//     vector<int> a = {4, 2, 2, 6, 4};
+//     int k = 6;
+//     int ans = subarraysWithXorK(a, k);
+//     cout << "The number of subarrays with XOR k is: "
+//          << ans << "\n";
+//     return 0;
+// }
+
 // int subarray(vector<int> &nums, int k)
 // {
 //     int n = nums.size();
