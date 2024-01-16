8847a550b0ca91fe4a7c08ef07840e273e4d38b8
rir7890 <Rahul Kumar>
2024-01-16 14:31:02 +0530

    get median and kth element from the 2 sorted array

--- practice_1.cpp
+++ practice_1.cpp
@@ -1,6 +1,174 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// int kthelement(vector<int> &a1, vector<int> &a2, int m, int n, int k)
+// {
+//     if (m > n)
+//     {
+//         return kthelement(a2, a1, n, m, k);
+//     }
+//     int low = max(0, k - m);
+//     int high = min(k, n);
+//     while (low <= high)
+//     {
+//         int cut1 = (low + high) >> 1;
+//         int cut2 = k - cut1;
+//         int l1 = cut1 == 0 ? INT_MIN : a1[cut1 - 1];
+//         int l2 = cut2 == 0 ? INT_MIN : a2[cut2 - 1];
+//         int r1 = cut1 == m ? INT_MAX : a1[cut1];
+//         int r2 = cut2 == n ? INT_MAX : a2[cut2];
+//         if (l1 <= r2 && l2 <= r1)
+//         {
+//             return max(l1, l2);
+//         }
+//         else if (l1 > r2)
+//         {
+//             high = cut1 - 1;
+//         }
+//         else
+//         {
+//             low = cut1 + 1;
+//         }
+//     }
+//     return 1;
+// }
+
+// int numbeSequence(int m, int n)
+// {
+//     if (m < n)
+//     {
+//         return 0;
+//     }
+//     if (n == 0)
+//     {
+//         return 0;
+//     }
+//     return numbeSequence(m / 2, n - 1) + numbeSequence(m - 1, n);
+// }
+
+// int solve(int ind, int n, int total, vector<int> &cost, vector<vector<int>> &dp)
+// {
+//     if (total <= 0)
+//     {
+//         return 0;
+//     }
+//     if (ind == n)
+//     {
+//         return 0;
+//     }
+//     if (dp[ind][total] != -1)
+//     {
+//         return dp[ind][total];
+//     }
+//     int notPick = solve(ind + 1, n, total, cost, dp);
+//     int pick = 0;
+//     if (cost[ind] <= total)
+//     {
+//         pick = 1 + solve(ind + 1, n, (total - cost[ind]) + (0.9 * cost[ind]), cost, dp);
+//     }
+//     return dp[ind][total] = max(pick, notPick);
+// }
+// int max_courses(int n, int total, vector<int> &cost)
+// {
+//     vector<vector<int>> dp(n, vector<int>(total + 1, -1));
+//     return solve(0, n, total, cost, dp);
+// }
+
+// vector<int> repeatedRows(vector<vector<int>> &matrix, int M, int N)
+// {
+//     vector<int> ans;
+//     unordered_map<string, int> um;
+//     for (int i = 0; i < M; i++)
+//     {
+//         string s = "";
+//         for (int j = 0; j < N; j++)
+//         {
+//             s += to_string(matrix[i][j]);
+//         }
+//         if (um.find(s) != um.end())
+//         {
+//             ans.push_back(i);
+//         }
+//         else
+//         {
+//             um[s] = i;
+//         }
+//     }
+//     if (ans.size() == 0)
+//     {
+//         return {-1};
+//     }
+//     return ans;
+// }
+// int main()
+// {
+//     vector<vector<int>> m{{1, 0, 0}, {1, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+//     int r = 4;
+//     int c = 3;
+//     for (const auto &v : repeatedRows(m, r, c))
+//     {
+//         std::cout << v << "\n";
+//     }
+//     return 0;
+// }
+
+// struct Node
+// {
+//     int data;
+//     Node *next;
+//     Node(int x) : data(x), next(NULL) {}
+// };
+// class LinkedLists
+// {
+// public:
+//     Node *head;
+//     Node *sorted;
+//     void push(int val)
+//     {
+//         Node *newnode = new Node(val);
+//         newnode->next = head;
+//         head = newnode;
+//     }
+//     void printList(Node *head)
+//     {
+//         while (head != NULL)
+//         {
+//             std::cout << head->data << " ";
+//             head = head->next;
+//         }
+//     }
+//     void insertSort(Node *headref)
+//     {
+//         sorted = NULL;
+//         Node *current = headref;
+//         while (current != NULL)
+//         {
+//             Node *next = current->next;
+//             sortedInsert(current);
+//             current = next;
+//         }
+//         head = sorted;
+//     }
+//     void sortedInsert(Node *newnode)
+//     {
+//         if (sorted == NULL || newnode->data <= sorted->data)
+//         {
+//             newnode->next = sorted;
+//             sorted = newnode;
+//         }
+//         else
+//         {
+//             Node *current = sorted;
+//             while (current->next && current->next->data < newnode->data)
+//             {
+//                 current = current->next;
+//             }
+//             newnode->next = current->next;
+//             current->next = newnode;
+//         }
+//     }
+// };
+
 // queue<int> modifyQueue(queue<int> &q, int k)
 // {
 //     stack<int> st;
